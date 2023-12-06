1a3af99357b01542893daeea50bb5ed5de171963
rowlandwilliams <rowland.williams.94@gmail.com>
2023-12-06 20:05:34 +0000

    add projects images

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { ProjectImages } from "@/components/ProjectImages/ProjectImages";
 import { ProjectDocument } from "@/graphql/generated";
 import { getUrqlClient } from "@/lib/urql";
-import Image from "next/image";
+import Link from "next/link";
 
 async function getProjectByName(projectName: string) {
   const { client } = getUrqlClient();
@@ -14,11 +14,20 @@ export default async function Project({
 }: {
   params: { slug: string };
 }) {
-  const { data, error } = await getProjectByName(params.slug);
+  const { data } = await getProjectByName(params.slug);
 
   const projectImages = data?.allProject[0].projectImages;
   return (
-    <div>
+    <div className="space-y-4">
+      <div className="flex items-center gap-x-2 text-sm">
+        <Link
+          href="/"
+          className="underline underline-offset-4 font-normal text-zinc-900 dark:text-zinc-300"
+        >
+          Projects
+        </Link>
+        /<div className="text-zinc-600 dark:text-zinc-400">{params.slug}</div>
+      </div>
       <ProjectImages projectImages={projectImages} />
     </div>
   );
--- src/components/ProjectImages/ProjectImages.tsx
+++ src/components/ProjectImages/ProjectImages.tsx
@@ -7,16 +7,18 @@ import Image from "next/image";
 import { useState } from "react";
 
 interface Props {
-  projectImages: ProjectFieldsFragment["projectImages"] | undefined;
+  projectImages: ProjectFieldsFragment["projectImages"];
 }
 
 export const ProjectImages = ({ projectImages }: Props) => {
   const [activeImage, setActiveImage] = useState(0);
 
-  const url = projectImages[activeImage]?.image?.asset?.url;
+  const url = projectImages
+    ? projectImages[activeImage]?.image?.asset?.url
+    : "";
 
-  const match = url?.match(/-(\d+)x(\d+)\./);
-  const width = parseInt(match[1], 10);
+  const match = url?.match(/-(\d+)x(\d+)\./) || "";
+  const width = match[1] ? parseInt(match[1], 10) : 0;
   const height = parseInt(match[2], 10);
 
   // Calculate aspect ratio
@@ -24,33 +26,33 @@ export const ProjectImages = ({ projectImages }: Props) => {
 
   const { graphWidth, ref } = useResponsiveGraphDims();
 
-  return (
+  return projectImages && projectImages[activeImage] ? (
     <section className="space-y-2">
-      {projectImages && projectImages[activeImage] ? (
-        <div ref={ref} key={projectImages[activeImage]?.image?.asset?.url}>
-          {projectImages[activeImage]?.image?.asset?.url && (
-            <Image
-              src={projectImages[activeImage]?.image?.asset?.url}
-              width={graphWidth}
-              height={graphWidth * aspectRatio}
-              alt=""
-              className="object-contain object-top rounded-sm"
-            />
-          )}
-        </div>
-      ) : null}
-      <div className="flex gap-x-2">
-        {projectImages?.map((image, i) => (
-          <button
-            onClick={() => setActiveImage(i)}
-            key={image?.caption}
-            className={classNames("w-[10px] h-[10px] rounded-full ", {
-              "bg-indigo-600": activeImage === i,
-              "bg-zinc-600": activeImage !== i,
-            })}
+      <div ref={ref} key={projectImages[activeImage]?.image?.asset?.url}>
+        {projectImages[activeImage]?.image?.asset?.url && (
+          <Image
+            src={projectImages[activeImage]?.image?.asset?.url as string}
+            width={graphWidth}
+            height={graphWidth * aspectRatio}
+            alt=""
+            className="object-contain object-top rounded-sm"
           />
-        ))}
+        )}
       </div>
+      {graphWidth > 0 && (
+        <div className="flex gap-x-2">
+          {projectImages?.map((image, i) => (
+            <button
+              onClick={() => setActiveImage(i)}
+              key={image?.caption}
+              className={classNames("w-[10px] h-[10px] rounded-full ", {
+                "bg-indigo-600": activeImage === i,
+                "bg-zinc-600": activeImage !== i,
+              })}
+            />
+          ))}
+        </div>
+      )}
     </section>
-  );
+  ) : null;
 };
